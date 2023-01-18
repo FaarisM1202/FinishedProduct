faa5d29429a0b77c3d8c6454ae9ef8cff117d2cd
Faaris Mohamed <mohamed8876@students.cptc.edu>
2023-01-17 21:14:08 -0800

    Add project files.

---
+++ NestedObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new();
+{
+    Email = "randomemail123@gmail.com",
+    FullName = "John Smith",
+    OfficeLocation = "B17 Rm 150"
+};
+
+Student stu1 = new();
+{
+    FirstName = "Jimmy",
+    LastName = "Neutron",
+    DateOfBirth = new DateTime(1999, 5, 14),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "12345678912",
+    SchoolEmail = "jimmy.neutron123@gmail.com"
+};
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
